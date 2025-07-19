8649f6878fe60ee0fd6491a18cf200be7f1afa59
Ann <annill13@mail.ru>
2025-07-19 17:24:05 +0300

    fixes

--- src/main/java/org/annill/contractor/dto/ContractorDto.java
+++ src/main/java/org/annill/contractor/dto/ContractorDto.java
@@ -1,10 +1,12 @@
 package org.annill.contractor.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder(toBuilder = true)
+@Schema
 public class ContractorDto {
 
     private String id;
